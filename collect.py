1378e9df92ec56111b48f8bb25835f462ebc7f5a
Diego Jahn <jahndiego@mailbox.org>
2020-10-29 23:43:17 +0100

    Read 'pub_date' from data. Add DB archive.
    - Changed data of the same day is updated instead of saved as a new series in "dresden_official".
    - Save all data sets with own series key in "dresden_official_all".

--- collect.py
+++ collect.py
@@ -7,7 +7,8 @@ JSON_URL = 'https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/co
 CACHED_JSON_FILENAME = 'cached.json'
 
 INFLUXDB_DATABASE = 'corona_dd'
-INFLUXDB_MEASUREMENT = 'dresden_official'
+INFLUXDB_MEASUREMENTS = ['dresden_official', 'dresden_official_all'] # all measurements to be saved
+INFLUXDB_MEASUREMENT_ARCHIVE = 'dresden_official_all' # measurements that contains all series (latest of the day as well as all corrections by the city)
 
 import sys
 
@@ -109,31 +110,42 @@ def main():
         with urllib.request.urlopen(JSON_URL) as response:
             data = json.load(response)
             logger.debug('Downloaded JSON data from server.')
+   
+    # get current date from system and latest entry date from the data set
+    data_load_date = datetime.utcnow()
+    midnight = data_load_date.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+    data_latest_date = datetime.fromtimestamp(data['features'][-1]['attributes']['Datum_neu']/1000, tz=timezone.utc) # date from last entry in data set
+            
         
-    # check whether downloaded JSON contains new data or user enforced data collection
+    # check whether downloaded JSON contains new data or user enforced data collection 
     if data == cached_data and not args.force_collect:
         logger.info('Data has not changed.')
     else:
         if data != cached_data:
-            logger.info('Found new data!')
+            if data_latest_date.timestamp() >= midnight.timestamp():
+                # last entry is dated today
+                logger.info('Data for today has changed!')
+            else:
+                # last date is dated before today
+                logger.info('Data has changed, but last entry is not from today.')
         else:
             logger.info('Data has not changed, but is nevertheless collected as requested.')
+
         # save query date
         if args.date:
             try:
-                data_pub_date = dateutil.parser.parse(args.date) # use user's publishing date if given for the new data set
+                data_load_date = dateutil.parser.parse(args.date) # use user's publishing date if given for the new data set
             except dateutil.parser.ParserError:
                 logger.error('Failed to parse publishing date \'{:s}\'.'.format(args.date))
                 sys.exit()
         elif args.file and not args.auto_date:
             json_filename = pathlib.Path(args.file.name).stem
             try:
-                data_pub_date = dateutil.parser.parse(json_filename) # try to parse the filename as date, if '--auto-date' option is set or data is loaded downloaded from server
+                data_load_date = dateutil.parser.parse(json_filename) # try to parse the filename as date, if '--auto-date' option is set or data is loaded downloaded from server
             except dateutil.parser.ParserError:
                 logger.error('Failed to parse the publishing date \'{:s}\' from filename. Please rename the file so that it has a valid date format or use the \'--date\' option to specify the date or pass \'--auto-date\' to save the current time as the publishing date for this time series. For further help type \'python {} --help\'.'.format(json_filename, pathlib.Path(__file__).resolve().name))
                 sys.exit()
-        else: # loaded from file with '--auto-date' option or downloaded from server
-            data_pub_date = datetime.now(tz=timezone.utc) # otherwise use current time
+        #else: # loaded from file with '--auto-date' option or downloaded from server
 
         # cache JSON file
         if not args.no_cache:
@@ -143,61 +155,67 @@ def main():
         if args.archive_json:
             archive_file_dir = pathlib.Path(abs_python_file_dir, 'json-archive')
             pathlib.Path.mkdir(archive_file_dir, exist_ok=True)
-            archive_file_path = pathlib.Path(archive_file_dir, '{:s}.json'.format(data_pub_date.strftime('%Y-%m-%dT%H:%M:%SZ')))
+            archive_file_path = pathlib.Path(archive_file_dir, '{:s}.json'.format(data_load_date.strftime('%Y-%m-%dT%H:%M:%SZ')))
             with open(archive_file_path, 'w') as json_file:
                 json.dump(data, json_file)
 
         # define tags of the time series
-        influxdb_tag_pub_date = data_pub_date.strftime('%Y-%m-%dT%H:%M:%S') # date on which the record was published
-        influxdb_tag_pub_date_short = data_pub_date.strftime('%d.%m.%Y') # shorter version for graph legend aliases in Grafana; https://grafana.com/docs/grafana/latest/datasources/influxdb/#alias-patterns
+        influxdb_pub_date = data_load_date.strftime('%Y-%m-%dT%H:%M:%S') # date on which the record was published
+        influxdb_tag_latest_date_short = data_latest_date.strftime('%d.%m.%Y') # shorter version for graph legend aliases in Grafana; https://grafana.com/docs/grafana/latest/datasources/influxdb/#alias-patterns
         influxdb_tag_script_version = RELEASE # state version number of this script
 
         # generate time series list according to the expected InfluxDB line protocol: https://docs.influxdata.com/influxdb/v1.8/write_protocols/line_protocol_tutorial/
-        time_series = []
-        for point in data['features']:
-            point_dict = {
-                'measurement'   : INFLUXDB_MEASUREMENT,
-                'tags'          : { # metadata for the data point
-                    'pub_date'          : influxdb_tag_pub_date,
-                    'pub_date_short'    : influxdb_tag_pub_date_short,
-                    'script_version'    : influxdb_tag_script_version,
-                },
-                'time'          : int(dateutil.parser.parse(point['attributes']['Datum'], dayfirst=True).replace(tzinfo=timezone.utc).timestamp()), # parse date, switch month and day, explicetely set UTC (InfluxDB uses UTC), otherwise local timezone is assumed; 'datetime.isoformat()': generate ISO 8601 formatted string (e. g. '2020-10-22T21:30:13.883657+00:00')
-                'fields'        : { # in principle, a simple "point.pop('attributes')" also works, but unfortunately the field datatype is defined by the first point written to a series (in case of this foreign data set, some fields are filled with NoneType); https://github.com/influxdata/influxdb/issues/3460#issuecomment-124747104
-                    # own fields
-                    'pub_date_seconds'              : int(data_pub_date.timestamp()), # add better searchable UNIX timestamp in seconds in addition to the human readable 'pub_date' tag; https://docs.influxdata.com/influxdb/v2.0/reference/glossary/#unix-timestamp; POSIX timestamps in Python: https://stackoverflow.com/a/8778548/7192373
-                    'Meldedatum_or_Zuwachs'         : int(point['attributes'].get('Fälle_Meldedatum', point['attributes']['Zuwachs_Fallzahl']) or 0), # Get the field 'Fälle_Meldedatum' that was introduced by the city on 29.10.2020, for older data sets use the field 'Zuwachs_Fallzahl'
-                    # fields from data source
-                    'Anzeige_Indikator'             : str(point['attributes']['Anzeige_Indikator']), # value is either None or 'x'
-                    'BelegteBetten'                 : int(point['attributes']['BelegteBetten'] or 0), # replace NoneType with 0
-                    'Datum'                         : str(point['attributes']['Datum']),
-                    'Datum_neu'                     : int(point['attributes']['Datum_neu'] or 0),
-                    'Fallzahl'                      : int(point['attributes']['Fallzahl'] or 0),
-                    'Fälle_Meldedatum'              : int(point['attributes'].get('Fälle_Meldedatum') or 0),
-                    'Genesungsfall'                 : int(point['attributes']['Genesungsfall'] or 0),
-                    'Hospitalisierung'              : int(point['attributes']['Hospitalisierung'] or 0),
-                    'Inzidenz'                      : float(point['attributes']['Inzidenz'] or 0),
-                    'ObjectId'                      : int(point['attributes']['ObjectId'] or 0),
-                    'Sterbefall'                    : int(point['attributes']['Sterbefall'] or 0),
-                    'Zeitraum'                      : str(point['attributes'].get('Zeitraum')),
-                    'Zuwachs_Fallzahl'              : int(point['attributes']['Zuwachs_Fallzahl'] or 0),
-                    'Zuwachs_Genesung'              : int(point['attributes']['Zuwachs_Genesung'] or 0),
-                    'Zuwachs_Krankenhauseinweisung' : int(point['attributes']['Zuwachs_Krankenhauseinweisung'] or 0),
-                    'Zuwachs_Sterbefall'            : int(point['attributes']['Zuwachs_Sterbefall'] or 0),
-                },
-            }
-            time_series.append(point_dict)
+        for influx_db_measurement in INFLUXDB_MEASUREMENTS:
+            time_series = []
+            for point in data['features']:
+                point_dict = {
+                    'measurement'   : influx_db_measurement,
+                    'tags'          : { # metadata for the data point
+                        'pub_date_short'    : influxdb_tag_latest_date_short, # legacy name for the date of the latest time series entry, not actually the publishing date
+                        'latest_date_short' : influxdb_tag_latest_date_short, # more accurate name for the date used
+                        'script_version'    : influxdb_tag_script_version,
+                    },
+                    'time'          : int(dateutil.parser.parse(point['attributes']['Datum'], dayfirst=True).replace(tzinfo=timezone.utc).timestamp()), # parse date, switch month and day, explicetely set UTC (InfluxDB uses UTC), otherwise local timezone is assumed; 'datetime.isoformat()': generate ISO 8601 formatted string (e. g. '2020-10-22T21:30:13.883657+00:00')
+                    'fields'        : { # in principle, a simple "point.pop('attributes')" also works, but unfortunately the field datatype is defined by the first point written to a series (in case of this foreign data set, some fields are filled with NoneType); https://github.com/influxdata/influxdb/issues/3460#issuecomment-124747104
+                        # own fields
+                        'pub_date'                      : influxdb_pub_date,
+                        'pub_date_seconds'              : int(data_load_date.timestamp()), # add better searchable UNIX timestamp in seconds in addition to the human readable 'pub_date' tag; https://docs.influxdata.com/influxdb/v2.0/reference/glossary/#unix-timestamp; POSIX timestamps in Python: https://stackoverflow.com/a/8778548/7192373
+                        'Meldedatum_or_Zuwachs'         : int(point['attributes'].get('Fälle_Meldedatum', point['attributes']['Zuwachs_Fallzahl']) or 0), # Get the field 'Fälle_Meldedatum' that was introduced by the city on 29.10.2020, for older data sets use the field 'Zuwachs_Fallzahl'
+                        # fields from data source
+                        'Anzeige_Indikator'             : str(point['attributes']['Anzeige_Indikator']), # value is either None or 'x'
+                        'BelegteBetten'                 : int(point['attributes']['BelegteBetten'] or 0), # replace NoneType with 0
+                        'Datum'                         : str(point['attributes']['Datum']),
+                        'Datum_neu'                     : int(point['attributes']['Datum_neu'] or 0),
+                        'Fallzahl'                      : int(point['attributes']['Fallzahl'] or 0),
+                        'Fälle_Meldedatum'              : int(point['attributes'].get('Fälle_Meldedatum') or 0),
+                        'Genesungsfall'                 : int(point['attributes']['Genesungsfall'] or 0),
+                        'Hospitalisierung'              : int(point['attributes']['Hospitalisierung'] or 0),
+                        'Inzidenz'                      : float(point['attributes']['Inzidenz'] or 0),
+                        'ObjectId'                      : int(point['attributes']['ObjectId'] or 0),
+                        'Sterbefall'                    : int(point['attributes']['Sterbefall'] or 0),
+                        'Zeitraum'                      : str(point['attributes'].get('Zeitraum')),
+                        'Zuwachs_Fallzahl'              : int(point['attributes']['Zuwachs_Fallzahl'] or 0),
+                        'Zuwachs_Genesung'              : int(point['attributes']['Zuwachs_Genesung'] or 0),
+                        'Zuwachs_Krankenhauseinweisung' : int(point['attributes']['Zuwachs_Krankenhauseinweisung'] or 0),
+                        'Zuwachs_Sterbefall'            : int(point['attributes']['Zuwachs_Sterbefall'] or 0),
+                    },
+                }
+                if influx_db_measurement == INFLUXDB_MEASUREMENT_ARCHIVE:
+                    # save every time series, including all corrections of the city of the same day, in an separate InfluxDB measurement, distiguishable by a 'pub_date' tag (containing exact date and time)
+                    point_dict['tags']['pub_date'] = influxdb_pub_date
 
-        # write data to database
-        db_client.write_points(time_series, time_precision='s')
+                time_series.append(point_dict)
+
+            # write data to database
+            db_client.write_points(time_series, time_precision='s')
 
         # do own calculations
-        # measurement that contains the daily 12 o'clock reports
+        # measurement that contains the daily 12 pm reports
         point_latest = time_series[-1]
 
         field_changes = { # convert some tags into fields (dict depth = 1)
-            'pub_date'      : point_latest['tags']['pub_date'],
-            'pub_date_short': point_latest['tags']['pub_date_short'],
+            'pub_date_short'    : point_latest['tags']['latest_date_short'], # legacy name for compatibility reasons
+            'latest_date_short' : point_latest['tags']['latest_date_short'], # more accurate name
         }
         point_latest['fields'].update(field_changes)
 
@@ -210,7 +228,8 @@ def main():
         point_latest.update(point_changes)
 
         db_client.write_points([point_latest], time_precision='s')
-        series_key = 'pub_date={:s},pub_date_short={:s},script_version={:s}'.format(influxdb_tag_pub_date, influxdb_tag_pub_date_short, influxdb_tag_script_version) # https://docs.influxdata.com/influxdb/v1.8/concepts/glossary/#series-key
+
+        series_key = 'latest_date_short={:s},script_version={:s}'.format(influxdb_tag_latest_date_short, influxdb_tag_script_version) # https://docs.influxdata.com/influxdb/v1.8/concepts/glossary/#series-key
         logger.info('Time series with tags \'{:s}\' successfully written to database.'.format(series_key))
 
 if __name__ == '__main__':
